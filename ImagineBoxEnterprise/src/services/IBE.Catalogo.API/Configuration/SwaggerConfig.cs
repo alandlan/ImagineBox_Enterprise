a450b791301fcb6a44c834044281477ffbff5c20
Alan Vieira Martins <alanhorcrux@gmail.com>
2021-12-08 19:20:12 -0300

    Configurando Swagger

--- ImagineBoxEnterprise/src/services/IBE.Catalogo.API/Configuration/SwaggerConfig.cs
+++ ImagineBoxEnterprise/src/services/IBE.Catalogo.API/Configuration/SwaggerConfig.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace IBE.Catalogo.API.Configuration
 {
-    public class SwaggerConfig
+    public static class SwaggerConfig
     {
+        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+           {
+               c.SwaggerDoc("v1", new OpenApiInfo()
+               {
+                   Title = "ImagineBox Enterprise Catálogo API",
+                   Description = "Esta API consulta o catalogo de produtos da ImagineBox",
+                   Contact = new OpenApiContact() { Name = "Alan Martins", Email = "alan4lann@gmail.com" },
+                   License = new OpenApiLicense() { Name = "MIT", Url = new Uri("www.google.com.br") }
+               });
+           });
+        }
+
+        public static void UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+        }
     }
 }
--- ImagineBoxEnterprise/src/services/IBE.Catalogo.API/Startup.cs
+++ ImagineBoxEnterprise/src/services/IBE.Catalogo.API/Startup.cs
@@ -1,20 +1,9 @@
 using IBE.Catalogo.API.Configuration;
-using IBE.Catalogo.API.Data;
-using IBE.Catalogo.API.Data.Repository;
-using IBE.Catalogo.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace IBE.Catalogo.API
 {
@@ -42,12 +31,15 @@ namespace IBE.Catalogo.API
         {
             services.AddApiConfiguration(Configuration);
 
+            services.AddSwaggerConfiguration();
+
             services.RegisterServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwaggerConfiguration();
             app.UseApiConfiguration(env);
         }
     }
