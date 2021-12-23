9dadba79007efbf5c8da8baae1641bb3475c8a85
Alan Vieira Martins <alanhorcrux@gmail.com>
2021-12-23 15:30:38 -0300

    ajustando swagger catalogo para ler token JWT

--- ImagineBoxEnterprise/src/services/IBE.Catalogo.API/Configuration/SwaggerConfig.cs
+++ ImagineBoxEnterprise/src/services/IBE.Catalogo.API/Configuration/SwaggerConfig.cs
@@ -21,6 +21,31 @@ namespace IBE.Catalogo.API.Configuration
                     Contact = new OpenApiContact() { Name = "Alan Martins", Email = "alan4lann@gmail.com" },
                     License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
                 });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
             });
         }
 
