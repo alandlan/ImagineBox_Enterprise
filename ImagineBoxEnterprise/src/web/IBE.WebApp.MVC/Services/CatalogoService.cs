5877a86d5b8cfccfcfab412f1b988875bd514f5a
Alan Vieira Martins <alanhorcrux@gmail.com>
2021-12-28 10:53:23 -0300

    Projeto Web - Service Catalogo

---
+++ ImagineBoxEnterprise/src/web/IBE.WebApp.MVC/Services/CatalogoService.cs
@@ -0,0 +1,40 @@
+﻿using IBE.WebApp.MVC.Extensions;
+using IBE.WebApp.MVC.Models;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IBE.WebApp.MVC.Services
+{
+    public class CatalogoService : Service, ICatalogoService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CatalogoService(HttpClient httpClient, IOptions<AppSettings> settings)
+        {
+            httpClient.BaseAddress = new System.Uri(settings.Value.AutenticacaoUrl);
+
+            _httpClient = httpClient;
+        }
+
+        public async Task<ProdutoViewModel> ObterPorId(Guid id)
+        {
+            var response = await _httpClient.GetAsync($"/catalogo/produtos/{id}");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<ProdutoViewModel>(response);
+        }
+
+        public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+        {
+            var response = await _httpClient.GetAsync("/catalogo/produtos");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<IEnumerable<ProdutoViewModel>>(response);
+        }
+    }
+}
---
+++ ImagineBoxEnterprise/src/web/IBE.WebApp.MVC/Services/ICatalogoService.cs
@@ -0,0 +1,15 @@
+﻿using IBE.WebApp.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IBE.WebApp.MVC.Services
+{
+    public interface ICatalogoService
+    {
+        Task<IEnumerable<ProdutoViewModel>> ObterTodos();
+
+        Task<ProdutoViewModel> ObterPorId(Guid id);
+    }
+}
