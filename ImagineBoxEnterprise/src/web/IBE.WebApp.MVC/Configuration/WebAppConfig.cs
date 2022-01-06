edd6b0e382f4526263dd04a909aaeaf7bdea1554
Alan Vieira Martins <alanhorcrux@gmail.com>
2022-01-06 16:06:11 -0300

    Projeto Web - Configurando Cultura pr-Br

--- ImagineBoxEnterprise/src/web/IBE.WebApp.MVC/Configuration/WebAppConfig.cs
+++ ImagineBoxEnterprise/src/web/IBE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -1,9 +1,11 @@
 ﻿using IBE.WebApp.MVC.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Globalization;
 
 namespace IBE.WebApp.MVC.Configuration
 {
@@ -38,6 +40,15 @@ namespace IBE.WebApp.MVC.Configuration
             app.UseRouting();
 
             app.UseIdentityConfiguration();
+
+            var supportedCultures = new[] { new CultureInfo("pt-BR") };
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("pt-BR"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+                
+            });
 
             app.UseMiddleware<ExceptionMiddleware>();
 
