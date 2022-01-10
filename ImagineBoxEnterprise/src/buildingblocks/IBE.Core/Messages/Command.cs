a6742031283746afe0c42eb7914a84765d928151
Alan Vieira Martins <alanhorcrux@gmail.com>
2022-01-10 15:55:31 -0300

    Projeto Core - Add FluentValidation

---
+++ ImagineBoxEnterprise/src/buildingblocks/IBE.Core/Messages/Command.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBE.Core.Messages
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public virtual bool EhValido()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ImagineBoxEnterprise/src/buildingblocks/IBE.Core/Messages/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBE.Core.Messages
+{
+    public abstract class Message
+    {
+        public string MessageType { get; protected set; }
+        public Guid AggredateId { get; protected set; }
+
+        protected Message()
+        {
+            MessageType = GetType().Name;
+        }
+    }
+}
