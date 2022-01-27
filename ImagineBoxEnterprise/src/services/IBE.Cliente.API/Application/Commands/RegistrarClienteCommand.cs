6057b33a4a3244988d94bb571bd981048a40fecd
Alan Vieira Martins <alanhorcrux@gmail.com>
2022-01-27 11:02:35 -0300

    Projeto Cliente API - Validação de Comandos

--- ImagineBoxEnterprise/src/services/IBE.Cliente.API/Application/Commands/RegistrarClienteCommand.cs
+++ ImagineBoxEnterprise/src/services/IBE.Cliente.API/Application/Commands/RegistrarClienteCommand.cs
@@ -1,4 +1,5 @@
-﻿using IBE.Core.Messages;
+﻿using FluentValidation;
+using IBE.Core.Messages;
 using System;
 
 namespace IBE.Cliente.API.Application.Commands
@@ -18,5 +19,45 @@ namespace IBE.Cliente.API.Application.Commands
             Email = email;
             Cpf = cpf;
         }
+
+        public override bool EhValido()
+        {
+            ValidationResult = new RegistrarClienteValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+
+        public class RegistrarClienteValidation : AbstractValidator<RegistrarClienteCommand>
+        {
+            public RegistrarClienteValidation()
+            {
+                RuleFor(c => c.Id)
+                    .NotEqual(Guid.Empty)
+                    .WithMessage("Id do cliente inválido");
+
+                RuleFor(c => c.Nome)
+                    .NotEmpty()
+                    .WithMessage("O nome do cliente não foi informado");
+
+                RuleFor(c => c.Cpf)
+                    .Must(TerCpfValidado)
+                    .WithMessage("O Cpf informado não é válido.");
+
+                RuleFor(c => c.Email)
+                    .Must(TerEmailValidado)
+                    .WithMessage("O e-mail informado não é válido");
+            }
+
+            protected static bool TerCpfValidado(string cpf)
+            {
+                return Core.DomainObjects.Cpf.Validar(cpf);
+            }
+
+            protected static bool TerEmailValidado(string email)
+            {
+                return Core.DomainObjects.Email.Validar(email);
+            }
+        }
     }
+
+    
 }
