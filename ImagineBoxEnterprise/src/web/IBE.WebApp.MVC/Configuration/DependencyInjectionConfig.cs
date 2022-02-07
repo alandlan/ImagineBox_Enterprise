97091d65c301ccbf5f826a9b398cdb6becd6c208
Alan Vieira Martins <alanhorcrux@gmail.com>
2022-02-07 14:00:03 -0300

    Projeto Web App - Validando CPF lado cliente

--- ImagineBoxEnterprise/src/web/IBE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ ImagineBoxEnterprise/src/web/IBE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -2,6 +2,7 @@
 using IBE.WebApp.MVC.Services;
 using IBE.WebApp.MVC.Services.Handlers;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Polly;
@@ -17,6 +18,8 @@ namespace IBE.WebApp.MVC.Configuration
     {
         public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddSingleton<IValidationAttributeAdapterProvider, CpfValidationAttributeAdapterProvider>();
+
             services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
 
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
--- ImagineBoxEnterprise/src/web/IBE.WebApp.MVC/Models/UsuarioViewModel.cs
+++ ImagineBoxEnterprise/src/web/IBE.WebApp.MVC/Models/UsuarioViewModel.cs
@@ -1,20 +1,34 @@
-﻿using System.Collections;
+﻿using IBE.WebApp.MVC.Extensions;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace IBE.WebApp.MVC.Models
 {
     public class UsuarioRegistro
     {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DisplayName("Nome Completo")]
+        public string Nome { get; set; }
+        
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DisplayName("CPF")]
+        [Cpf]
+        public string Cpf { get; set; }
+
         [Required(ErrorMessage = "O Campo {0} é obrigatório")]
         [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        [DisplayName("Email")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "O Campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = "O Campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        [DisplayName("Senha")]
         public string Senha { get; set; }
 
         [Compare("Senha", ErrorMessage = "As senhas não conferem.")]
+        [DisplayName("Confirme sua senha")]
         public string SenhaConfirmacao { get; set; }
     }
 
